59a33a53e15ad2099ff46334b5279c6872f9fe39
lilyclements <lily@idems.international>
2023-01-23 11:08:46 +0000

    code to create shiny

---
+++ R Code/create_data.R
@@ -0,0 +1,40 @@
+library(rapidpror)
+
+set_rapidpro_key("f6a2ce1ada103b40190fc5930fd430b28be62370")
+set_rapidpro_site("https://rapidpro-next.idems.international/api/v2/")
+user_data <- get_data_from_rapidpro_api(call_type = "contacts.json",
+                                        date_from = NULL,
+                                        date_to = NULL)
+
+update_data <- function(){
+  # Create the various data sets
+  # station metadata data
+  station_name <- user_data$fields$station_name
+  station_type <- user_data$fields$type_of_station
+  station_element <- user_data$fields$station_element
+  station_df <- data.frame(station_name, station_type, station_element)
+  station_df <- unique(station_df)
+  
+  rain_df <- element_data(user_data$fields$rainfall_measurements, "Rain")
+  tmin_df <- element_data(user_data$fields$tmin_measurements, "Min Temp")
+  tmax_df <- element_data(user_data$fields$tmax_measurements, "Max Temp")
+  
+  objects_to_return <- NULL
+  objects_to_return[[1]] <- station_df
+  objects_to_return[[2]] <- rain_df
+  objects_to_return[[3]] <- tmin_df
+  objects_to_return[[4]] <- tmax_df
+  return(objects_to_return)
+}
+
+
+# 
+# x <- c("datedate, 22, C, edited, date2, 24, U, edited,",
+#        "datedate, 22, C, edited, edited, edited,",
+#        "datedate, 22, C, edited, date2, 24, U, edited, date3, 26, C",
+#        "datedate, 22, C")
+# 
+# # for that station
+# vars_to_check <- grep("edited,", x)
+# edit_correction(x, vars_to_check)
+
---
+++ R Code/functions.R
@@ -0,0 +1,160 @@
+#' Interaction with api
+#' Description: Make API call, specify query string, save data in dataframe
+
+#' 1. Defining package environment -------------------------------------------
+#' Sorting set and get calls for: key, site, uuid flow names
+#' 
+#' Define package environment
+utils::globalVariables(c("pkg_env"))
+pkg_env <- new.env(parent = emptyenv())
+pkg_env$rapidpro_key <- NULL
+pkg_env$rapidpro_site <- NULL
+pkg_env$rapidpro_uuid_names <- NULL 
+
+set_rapidpro_key = function(key) {
+  if (!is.character(key)){
+    stop("`key` provided should be a character variable")
+  }
+  pkg_env$rapidpro_key <- key 
+}
+
+set_rapidpro_site = function(site) {
+  if (!is.character(site)){
+    stop("`site` provided should be a character variable")
+  }
+  pkg_env$rapidpro_site <- site 
+}
+
+set_rapidpro_uuid_names = function(uuid_names = get_flow_names()){
+  pkg_env$rapidpro_uuid_names <- uuid_names 
+}
+
+get_rapidpro_key = function() {
+  get("rapidpro_key", envir = pkg_env)
+}
+
+get_rapidpro_site = function() {
+  get("rapidpro_site", envir = pkg_env)
+}
+
+get_rapidpro_uuid_names = function(){
+  get("rapidpro_uuid_names", envir = pkg_env)
+}
+
+get_user_data <- function(rapidpro_site = get_rapidpro_site(), token = get_rapidpro_key(), flatten = FALSE, date_from = NULL, date_to = NULL, format_date = "%Y-%m-%d", tzone_date = "UTC"){
+  user_data <- get_data_from_rapidpro_api(call_type = "contacts.json?group=joined", rapidpro_site = rapidpro_site, token = token, flatten = flatten, date_from = NULL, date_to = NULL, format_date = format_date, tzone_date = tzone_date)
+  
+  if (!flatten){
+    if (!is.null(date_from)){
+      user_data <- user_data %>% dplyr::filter(as.POSIXct(date_from, format=format_date, tzone = tzone_date) < as.POSIXct(user_data$created_on, format="%Y-%m-%dT%H:%M:%OS", tz = "UTC"))
+    }
+    if (!is.null(date_to)){
+      user_data <- user_data %>% dplyr::filter(as.POSIXct(date_to, format=format_date, tzone = tzone_date) > as.POSIXct(user_data$created_on, format="%Y-%m-%dT%H:%M:%OS", tz = "UTC"))
+    }
+  } else {
+    if (!is.null(date_from)){
+      user_data <- user_data %>% dplyr::filter(as.POSIXct(date_from, format=format_date, tzone = tzone_date) < as.POSIXct(user_data$fields.starting_date, format="%Y-%m-%dT%H:%M:%OS", tz = "UTC"))
+    }
+    if (!is.null(date_to)){
+      user_data <- user_data %>% dplyr::filter(as.POSIXct(date_to, format=format_date, tzone = tzone_date) > as.POSIXct(user_data$fields.starting_date, format="%Y-%m-%dT%H:%M:%OS", tz = "UTC"))
+    }
+  }
+  return(user_data)
+}
+
+get_data_from_rapidpro_api <- function(call_type, rapidpro_site = get_rapidpro_site(), token = get_rapidpro_key(), flatten = FALSE,
+                                       date_from, date_to, format_date = "%Y-%m-%d", tzone_date = "UTC"){
+  if (is.null(rapidpro_site)){
+    stop("rapidpro_site is NULL. Set a website with `set_rapidpro_site`.")
+  }
+  if (is.null(token)){
+    stop("token is NULL. Set a token with `set_rapidpro_key`.")
+  }
+  if (is.null(call_type)){
+    stop("call_type is NULL. Expecting a valid call_type.")
+  }
+  if (!is.logical(flatten)){
+    stop("flatten should be TRUE or FALSE")
+  }
+  get_command <- paste(rapidpro_site, call_type, sep = "")
+  user_result <- httr_get_call(get_command = get_command, token = token)
+  if (flatten){
+    user_result <- jsonlite::flatten(user_result)
+  }
+  if (!is.null(date_from)){
+    user_result <- user_result %>% dplyr::filter(as.POSIXct(date_from, format=format_date, tzone = tzone_date) < as.POSIXct(user_result$created_on, format="%Y-%m-%dT%H:%M:%OS", tz = "UTC"))
+  }
+  if (!is.null(date_to)){
+    user_result <- user_result %>% dplyr::filter(as.POSIXct(date_to, format=format_date, tzone = tzone_date) > as.POSIXct(user_result$created_on, format="%Y-%m-%dT%H:%M:%OS", tz = "UTC"))
+  }
+  return(user_result)
+}
+
+
+# New Functions for Climatic ShinyBot -----------------------------------------------------
+
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+
+# if it says edited, but ends on "edited," then remove "edited,"
+# if it says edited, but does not end on "edited,", then remove everything before "edited,"
+edit_correction <- function(x, vars_to_check){
+  for (i in c(vars_to_check)){
+    if (substrRight(x[i], 7) == "edited,"){
+      # remove "edited," - removes last one
+      x[i] <- substring(x[i], 1, nchar(x[i])-8)
+      
+      # recursive function here now, in case of other edited,'s.
+      if (grepl("edited,", x[i])){
+        y <- x[i]
+        x[i] <- edit_correction(y, 1)
+      } else {
+        x[i] <- x[i]
+      }
+    } else {
+      # remove everything before the final "edited,"
+      x[i] <- gsub(".*edited,","",x[i])
+    }
+    
+  }
+  return(x)
+}
+
+element_data <- function(element_col = user_data$fields$rainfall_measurements,
+                         element_name = "Rain"){
+  element_measure <- element_col
+  element_split <- stringr::str_split(element_measure, pattern = stringr::fixed("|"))
+  names(element_split) <- user_data$fields$station_name
+  for (i in 1:length(element_split)){
+    x <- element_split[[i]]
+    # which elements have "edited" in them?
+    vars_to_check <- grep("edited", x)
+    x <- edit_correction(x, vars_to_check)
+    x <- x[x!=""]
+    element_split[[i]] <- data.frame(stringr::str_split(x, pattern = stringr::fixed(","), simplify = TRUE))
+  }
+  element_data <- plyr::ldply(element_split, `.id` = "Station") %>%
+    mutate(Type = element_name) %>%
+    relocate(Type, .after = Station)
+  # remove columns and rows that contain all NAs
+  element_data <- element_data %>% 
+    dplyr::select(where(function(x) any(!is.na(x)))) %>%
+    filter(rowSums(is.na(element_data)) != (length(element_data)-1)) # minus 1 because all have the type column
+  if (length(element_data) == 5){
+    names(element_data)[3:5] <- c("Entry Time", "Measurement", "Confirmed")
+    element_data$`Entry Time` <- gsub("\\..*","", element_data$`Entry Time`)
+    element_data <- element_data %>% filter(Confirmed != "D") # TODO: should do this earlier in the code
+  }
+  return(element_data)
+}
+
+raw_data <- function(rapidpro_site = get_rapidpro_site(), token = get_rapidpro_key()){
+  get_command <- paste(rapidpro_site, "contacts.json", sep = "")
+  response <- httr::GET(get_command,
+                        config = httr::add_headers(Authorization = paste("Token", token)))
+  raw <- httr::content(response, as = "text")
+  return(raw)
+}
+
+
